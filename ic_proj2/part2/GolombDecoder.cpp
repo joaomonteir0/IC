f3d3bf45730c1090327fa0f1d67023bf6b56aac9
jsousa11 <jsousa11@ua.pt>
2025-01-10 22:27:44 +0000

    Tudo nice

--- ic_proj2/part2/GolombDecoder.cpp
+++ ic_proj2/part2/GolombDecoder.cpp
@@ -3,172 +3,123 @@
 #include <string>
 #include <sndfile.hh>
 #include "Golomb.h"
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
-constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
+constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer para leitura de frames
 
-int main( int argc, char** argv ) {
-
-    //function that predicts the next value in the sequence based on 3 previous values
+int main(int argc, char** argv) {
     auto predict = [](int a, int b, int c) {
-        //3*a - 3*b + c
-        return 3*a - 3*b + c;
+        return 3 * a - 3 * b + c;
     };
 
-    //start a timer
     clock_t start = clock();
 
-    if(argc < 3) {
-		cerr << "Usage: " << argv[0] << " <input file> <output file>\n";
-		return 1;
-	}
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input file> <output file>\n";
+        return 1;
+    }
 
     int sampleRate = 44100;
-    BitStream bs (argv[1], "r");
-    vector<int> v_channels = bs.readBits(16);
-    vector<int> v_padding = bs.readBits(16);
-    vector<int> v_q = bs.readBits(16);
-    vector<int> v_nFrames = bs.readBits(32);
-    vector<int> v_blockSize = bs.readBits(16);
-    vector<int> v_num_zeros = bs.readBits(16);
-    vector<int> v_m_size = bs.readBits(16);
+    BitStream bs(argv[1], "r");
 
-    int nChannels = 0;
-    for(long unsigned int i = 0; i < v_channels.size(); i++)
-        nChannels += v_channels[i] * pow(2, v_channels.size() - i - 1);
-
-    int padding = 0;
-    for(long unsigned int i = 0; i < v_padding.size(); i++)
-        padding += v_padding[i] * pow(2, v_padding.size() - i - 1);
-
-    int q = 0;
-    for(long unsigned int i = 0; i < v_q.size(); i++)
-        q += v_q[i] * pow(2, v_q.size() - i - 1);
-
-    int nFrames = 0;
-    for(long unsigned int i = 0; i < v_nFrames.size(); i++)
-        nFrames += v_nFrames[i] * pow(2, v_nFrames.size() - i - 1);
-
-    SndfileHandle sfhOut { argv[argc-1], SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, nChannels, sampleRate };
-
-    int m_size = 0;
-    for(long unsigned int i = 0; i < v_m_size.size(); i++) {
-        m_size += v_m_size[i] * pow(2, v_m_size.size() - i - 1);
-    }
-
-    int blockSize = 0;
-    for(long unsigned int i = 0; i < v_blockSize.size(); i++) {
-        blockSize += v_blockSize[i] * pow(2, v_blockSize.size() - i - 1);
-    }
-
-    int num_zeros = 0;
-    for(long unsigned int i = 0; i < v_num_zeros.size(); i++) {
-        num_zeros += v_num_zeros[i] * pow(2, v_num_zeros.size() - i - 1);
-    }
-    
-    vector<int> m_vector;
-    for(int i = 0; i < m_size; i++) {
-        vector<int> v_m_i = bs.readBits(16);
-        int m_i = 0;
-        for(long unsigned int j = 0; j < v_m_i.size(); j++) {
-            m_i += v_m_i[j] * pow(2, v_m_i.size() - j - 1);
+    auto readHeader = [&](size_t numBits) {
+        vector<int> bits = bs.readBits(numBits);
+        int value = 0;
+        for (size_t i = 0; i < bits.size(); ++i) {
+            value += bits[i] * pow(2, bits.size() - i - 1);
         }
-        m_vector.push_back(m_i);
+        return value;
+    };
+
+    int nChannels = readHeader(16);
+    int padding = readHeader(16);
+    int q = readHeader(16);
+    size_t nFrames = static_cast<size_t>(readHeader(32));
+    int blockSize = readHeader(16);
+    int num_zeros = readHeader(16);
+    int m_size = readHeader(16);
+
+    SndfileHandle sfhOut(argv[2], SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, nChannels, sampleRate);
+
+    vector<int> m_vector(m_size);
+    for (int i = 0; i < m_size; ++i) {
+        m_vector[i] = readHeader(16);
     }
 
-    //total size of the file - the size of the header and the size of the m vector
-    int total = bs.getFileSize() - (16 + 2*m_size);
-    long totalBits = total*8;
-    vector<int> v_encoded = bs.readBits(totalBits);
-    //convert vector<int> of bits to string of bits
+    int totalBits = (bs.getFileSize() - (16 + 2 * m_size)) * 8;
+    vector<int> encodedBits = bs.readBits(totalBits);
+
     string encoded = "";
-    for(long unsigned int  i = 0; i < v_encoded.size(); i++) {
-        encoded += to_string(v_encoded[i]);
+    for (int bit : encodedBits) {
+        encoded += to_string(bit);
     }
-
-    //discard the last num_zeros bits
     encoded = encoded.substr(0, encoded.size() - num_zeros);
 
-    //decode looping through the v_m vector
     Golomb g;
-    vector<int> decoded;
-    if (m_size == 1)
-        decoded = g.decode(encoded, m_vector[0]);
-    else 
-        decoded = g.decodeMultiple(encoded, m_vector, blockSize);
+    vector<int> decoded = (m_size == 1) ? g.decode(encoded, m_vector[0]) : g.decodeMultiple(encoded, m_vector, blockSize);
 
     vector<short> samplesVector;
 
-    if (nChannels < 2) {
-        for (long unsigned int i = 0; i < decoded.size(); i++) {
+    if (nChannels == 1) {
+        for (size_t i = 0; i < decoded.size(); ++i) {
             if (i >= 3) {
-                int difference = decoded[i] + predict(samplesVector[i-1], samplesVector[i-2], samplesVector[i-3]);
-                samplesVector.push_back(difference);
+                int value = decoded[i] + predict(samplesVector[i - 1], samplesVector[i - 2], samplesVector[i - 3]);
+                samplesVector.push_back(value);
+            } else {
+                samplesVector.push_back(decoded[i]);
             }
-            else samplesVector.push_back(decoded[i]);
         }
     } else {
-        //da para otimizar apenas com um ciclo e condicionando o i
-        for (int i = 0; i < nFrames; i++) {
+        for (size_t i = 0; i < nFrames; ++i) {
             if (i >= 3) {
-                int difference = decoded[i] + predict(samplesVector[i-1], samplesVector[i-2], samplesVector[i-3]);
-                samplesVector.push_back(difference);
+                int value = decoded[i] + predict(samplesVector[i - 1], samplesVector[i - 2], samplesVector[i - 3]);
+                samplesVector.push_back(value);
+            } else {
+                samplesVector.push_back(decoded[i]);
             }
-            else samplesVector.push_back(decoded[i]); 
         }
 
-        for (long unsigned int i = nFrames; i < decoded.size(); i++) {
-            if ((int) i >= nFrames + 3) {
-                int difference = decoded[i] + predict(samplesVector[i-1], samplesVector[i-2], samplesVector[i-3]);
-                samplesVector.push_back(difference);
+        for (size_t i = nFrames; i < decoded.size(); ++i) {
+            if (i >= nFrames + 3) {
+                int value = decoded[i] + predict(samplesVector[i - 1], samplesVector[i - 2], samplesVector[i - 3]);
+                samplesVector.push_back(value);
+            } else {
+                samplesVector.push_back(decoded[i]);
             }
-            else samplesVector.push_back(decoded[i]); 
         }
 
-        //merge the two channels into one vector
         vector<short> merged;
-        //the first channel is the first nFrames samples
-        vector<short> firstChannel(samplesVector.begin(), samplesVector.begin() + nFrames);
-        //the second channel is the last nFrames samples
-        vector<short> secondChannel(samplesVector.begin() + nFrames, samplesVector.end());
-        for(int i = 0; i < nFrames; i++) {
-            merged.push_back(firstChannel[i]);
-            merged.push_back(secondChannel[i]);
+        vector<short> leftChannel(samplesVector.begin(), samplesVector.begin() + nFrames);
+        vector<short> rightChannel(samplesVector.begin() + nFrames, samplesVector.end());
+
+        for (size_t i = 0; i < nFrames; ++i) {
+            merged.push_back(leftChannel[i]);
+            merged.push_back(rightChannel[i]);
         }
         samplesVector = merged;
     }
 
-    //quantize the samples
     if (q != 0) {
-        if(q != 1){
-            for (long unsigned int i = 0; i < samplesVector.size(); i++) {
-                //shift the sample to the left by 1 bit
-                samplesVector[i] = samplesVector[i] << 1;
-                //change that last bit to 1
-                samplesVector[i] = samplesVector[i] | 1;
-                // //shif the sample to the left by q-1 bits
-                samplesVector[i] = samplesVector[i] << (q-1);
-            }
-        } else {
-            for (long unsigned int i = 0; i < samplesVector.size(); i++) {
-                //shift the sample to the left by 1 bit
-                samplesVector[i] = samplesVector[i] << 1;
+        for (auto& sample : samplesVector) {
+            if (q > 1) {
+                sample = (sample << (q - 1)) | 1;
             }
         }
     }
 
-    //remove the last int padding values
-    samplesVector = vector<short>(samplesVector.begin(), samplesVector.end() - padding);
-    //write to wav file the samples vector
+    if (padding > 0) {
+        samplesVector.resize(samplesVector.size() - padding);
+    }
+
     sfhOut.write(samplesVector.data(), samplesVector.size());
     bs.close();
 
-    //end timer
     clock_t end = clock();
-    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
-    //in milliseconds
-    cout << "Time elapsed: " << elapsed_secs*1000 << " ms" << endl;
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC * 1000;
+    cout << "Time elapsed: " << elapsed_secs << " ms" << endl;
 
     return 0;
 }
--- ic_proj2/part2/GolombEncoder.cpp
+++ ic_proj2/part2/GolombEncoder.cpp
@@ -1,40 +1,38 @@
 #include "Golomb.h"
 #include <iostream>
 #include <string>
-#include <string.h>
 #include <vector>
 #include <fftw3.h>
 #include <sndfile.hh>
 #include "../part1/BitStream.h"
+#include <string.h>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
 
 constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 
+// Function to predict the next value in the sequence based on 3 previous values
+int predict(int a, int b, int c) {
+    return 3 * a - 3 * b + c;
+}
+
+// Function to calculate m based on u
+int calc_m(int u) {
+    return static_cast<int>(-1 / log(static_cast<double>(u) / (1 + u)));
+}
+
 int main(int argc, char *argv[]) {
-
-    //function that predicts the next value in the sequence based on 3 previous values
-    auto predict = [](int a, int b, int c) {
-        //3*a - 3*b + c
-        return 3*a - 3*b + c;
-    };
-
-    //function to calculate m based on u
-    auto calc_m = [](int u) {
-        //u = alpha / 1 - alpha
-        //m = - (1 / log(alpha))
-        return (int) - (1/log((double) u / (1 + u)));
-    };
-
     if (argc < 4 || argc > 6) {
-		cerr << "Usage: " << argv[0] << " <input file> <output file> <m | bs> [auto] [q] \n";
-		return 1;
-	}
+        cerr << "Usage: " << argv[0] << " <input file> <output file> <m | bs> [auto] [q] \n";
+        return 1;
+    }
 
     int q = 0;
-    //check if "auto" is passed in
     bool autoMode = false;
     bool quantization = false;
+
     if (argc >= 5) {
         if (strcmp(argv[4], "auto") == 0) {
             autoMode = true;
@@ -54,53 +52,38 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    SndfileHandle sfhIn { argv[1] };
-	if(sfhIn.error()) {
-		cerr << "Error: invalid input file\n";
-		return 1;
+    SndfileHandle sfhIn{argv[1]};
+    if (sfhIn.error() || (sfhIn.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV || 
+        (sfhIn.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
+        cerr << "Error: invalid input file or format\n";
+        return 1;
     }
-    if((sfhIn.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV) {
-		cerr << "Error: file is not in WAV format\n";
-		return 1;
-	}
-	if((sfhIn.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
-		cerr << "Error: file is not in PCM_16 format\n";
-		return 1;
-	}
 
     string output = argv[2];
     short m = atoi(argv[3]);
-
-    //start a timer
     clock_t start = clock();
     short bs = m;
-    short og = m;
 
-    size_t nFrames { static_cast<size_t>(sfhIn.frames()) };
-    size_t nChannels { static_cast<size_t>(sfhIn.channels()) };
+    size_t nFrames = static_cast<size_t>(sfhIn.frames());
+    size_t nChannels = static_cast<size_t>(sfhIn.channels());
     vector<short> samples(nChannels * nFrames);
-	sfhIn.readf(samples.data(), nFrames);
-	size_t nBlocks { static_cast<size_t>(ceil(static_cast<double>(nFrames) / bs)) };
+    sfhIn.readf(samples.data(), nFrames);
+    size_t nBlocks = static_cast<size_t>(ceil(static_cast<double>(nFrames) / bs));
 
-    // Do zero padding, if necessary
-	samples.resize(nBlocks * bs * nChannels);
-
-    //calculate the padding done to the end of the file
+    samples.resize(nBlocks * bs * nChannels);
     int padding = samples.size() - nFrames * nChannels;
 
     vector<short> left_samples(samples.size() / 2);
     vector<short> right_samples(samples.size() / 2);
 
     if (quantization) {
-        for (long unsigned int i = 0; i < samples.size(); i++) {
-            //remove the q least significant bits
-            samples[i] = samples[i] >> q;
+        for (auto &sample : samples) {
+            sample >>= q; // Remove the q least significant bits
         }
     }
 
     if (nChannels > 1) {
-        //split samples into left and right channels
-        for (long unsigned int i = 0; i < samples.size()/2; i++) {
+        for (size_t i = 0; i < samples.size() / 2; ++i) {
             left_samples[i] = samples[i * nChannels];
             right_samples[i] = samples[i * nChannels + 1];
         }
@@ -108,173 +91,91 @@ int main(int argc, char *argv[]) {
 
     vector<int> m_vector;
     vector<int> valuesToBeEncoded;
-    // cout << "Predicting..." << endl;
+
+    auto process_samples = [&](const vector<short>& channel_samples) {
+        for (size_t i = 0; i < channel_samples.size(); ++i) {
+            if (i >= 3) {
+                int difference = channel_samples[i] - predict(channel_samples[i - 1], channel_samples[i - 2], channel_samples[i - 3]);
+                valuesToBeEncoded.push_back(difference);
+            } else {
+                valuesToBeEncoded.push_back(channel_samples[i]);
+            }
+
+            if (i % bs == 0 && i != 0) {
+                int sum = accumulate(valuesToBeEncoded.end() - bs, valuesToBeEncoded.end(), 0, [](int acc, int val) { return acc + abs(val); });
+                int u = round(sum / static_cast<double>(bs));
+                m = calc_m(u);
+                m = static_cast<short>(max(static_cast<int>(m), 1));
+                m_vector.push_back(m);
+            }
+        }
+    };
+
     if (nChannels < 2) {
-        for(long unsigned int i = 0; i < samples.size(); i++) {
-            if (i >= 3) {
-                int difference = samples[i] - predict(samples[i-1], samples[i-2], samples[i-3]);
-                valuesToBeEncoded.push_back(difference);
-            } else {
-                valuesToBeEncoded.push_back(samples[i]);
-            }
-        }
+        process_samples(samples);
     } else {
-        for (long unsigned int i = 0; i < left_samples.size(); i++) {
-            if (i >= 3) {
-                int difference = left_samples[i] - predict(left_samples[i-1], left_samples[i-2], left_samples[i-3]);
-                valuesToBeEncoded.push_back(difference);
-            } else {
-                valuesToBeEncoded.push_back(left_samples[i]);
-            }
-
-            //calculate m every bs samples
-            if (i % bs == 0 && i != 0) {
-                int sum = 0;
-                for (long unsigned int j = i-bs; j < i; j++) {
-                    sum += abs(valuesToBeEncoded[j]);
-                }
-                int u = round(sum/bs);
-                m = calc_m(u);
-                if (m < 1) m = 1;
-                m_vector.push_back(m);  
-            }
-            if (i == left_samples.size() - 1) {
-                int sum = 0;
-                for (long unsigned int j = i - (i % bs); j < i; j++) {
-                    sum += abs(valuesToBeEncoded[j]);
-                }
-                int u = round(sum/(i % bs));
-                m = calc_m(u);
-                if (m < 1) m = 1;
-                m_vector.push_back(m);
-            }
-        }
-
-        for(long unsigned int i = 0; i < left_samples.size(); i++){
-            if (i >= 3) {
-                int difference = right_samples[i] - predict(right_samples[i-1], right_samples[i-2], right_samples[i-3]);
-                valuesToBeEncoded.push_back(difference);
-            }
-            
-            else valuesToBeEncoded.push_back(right_samples[i]);
-
-            //calculate m every bs samples
-            if (i % bs == 0 && i != 0) {
-                int sum = 0;
-                for (long unsigned int j = i-bs; j < i; j++) {
-                    sum += abs(valuesToBeEncoded[j]);
-                }
-                int u = round(sum/bs);
-                m = calc_m(u);
-                if (m < 1) m = 1;
-                m_vector.push_back(m);
-            }
-            if (i == left_samples.size() - 1) {
-                int sum = 0;
-                for (long unsigned int j = i - (i % bs); j < i; j++) {
-                    sum += abs(valuesToBeEncoded[j]);
-                }
-                int u = round(sum/(i % bs));
-                m = calc_m(u);
-                if (m < 1) m = 1;
-                m_vector.push_back(m);
-            }
-        }
+        process_samples(left_samples);
+        process_samples(right_samples);
     }
 
-    string encodedString = "";
+    string encodedString;
     Golomb g;
 
-    if (!autoMode){
-        for(long unsigned int i = 0; i < valuesToBeEncoded.size() ; i++)
-            encodedString += g.encode(valuesToBeEncoded[i], og);
-    } else{
+    if (!autoMode) {
+        for (const auto &value : valuesToBeEncoded) {
+            encodedString += g.encode(value, m);
+        }
+    } else {
         int m_index = 0;
-        for (long unsigned int i = 0; i < valuesToBeEncoded.size(); i++) {
-            // cout << valuesToBeEncoded[i] << endl;   
+        for (size_t i = 0; i < valuesToBeEncoded.size(); ++i) {
             if (i % bs == 0 && i != 0) m_index++;
-            encodedString += g.encode(valuesToBeEncoded[i], m_vector[m_index]);    
+            encodedString += g.encode(valuesToBeEncoded[i], m_vector[m_index]);
         }
     }
 
     BitStream bitStream(output, "w");
     vector<int> bits;
-    vector<int> encoded_bits;
+    vector<int> encoded_bits(encodedString.begin(), encodedString.end());
     
-    //the next bits will be the encoded string
-    for(long unsigned int i = 0; i < encodedString.length(); i++)
-        encoded_bits.push_back(encodedString[i] - '0');
-    
-    //if the encoded string is not a multiple of 8, add 0s to the end of the string
-    //until it is a multiple of 8
-    int count_zeros = 0;
-    while (encoded_bits.size() % 8 != 0) {
-        encoded_bits.push_back(0);
-        count_zeros++;
+    // Convert encoded string to bits
+    for (auto &bit : encoded_bits) {
+        bit -= '0';
     }
 
-    //the first 16 bits of the file are the number of channels
-    for(int i = 15; i >= 0; i--) {
-        bits.push_back((sfhIn.channels() >> i) & 1);
-    }
+    int count_zeros = (8 - (encoded_bits.size() % 8)) % 8;
+    encoded_bits.insert(encoded_bits.end(), count_zeros, 0);
 
-    //the next 16 bits of the file are the padding
-    for(int i = 15; i >= 0; i--) {
-        bits.push_back((padding >> i) & 1);
-    }
-
-    //the next 16 bits of the file are the quantization factor
-    for(int i = 15; i >= 0; i--) {
-        bits.push_back((q >> i) & 1);
-    }
-    // cout << "channels: " << sfhIn.channels() << endl;
-
-    //the next 32 bits of the file are the number of frames
-    for(int i = 31; i >= 0; i--) {
-        bits.push_back((samples.size()/2 >> i) & 1);
-    }
-    // cout << "nFrames: " << nFrames << endl;
-
-    //the next 16 bits is the block size
-    for(int i = 15; i >= 0; i--) {
-        bits.push_back((bs >> i) & 1);
-    }
-    // cout << "block size: " << bs << endl;
-
-    //the next 16 bits is the number of zeros added to the end of the encoded string
-    for(int i = 15; i >= 0; i--) {
-        bits.push_back((count_zeros >> i) & 1);
-    }
-    // cout << "number of zeros added: " << count_zeros << endl;
-
-    //the next 16 bits of the output file will be size of m_vector converted to binary
-    if (!autoMode){
-        m_vector.clear();
-        m_vector.push_back(og);
-    }   
-    for (int i = 15; i >= 0; i--){
-        bits.push_back((m_vector.size() >> i) & 1);
-    }
-
-    // the next bits will be the values of m_vector converted to binary
-    for (long unsigned int i = 0; i < m_vector.size(); i++) {
-        for (int j = 15; j >= 0; j--) {
-            bits.push_back((m_vector[i] >> j) & 1);
+    // Prepare header bits
+    auto add_bits = [&](int value, int bit_count) {
+        for (int i = bit_count - 1; i >= 0; --i) {
+            bits.push_back((value >> i) & 1);
         }
-    }
-    
-    //the next bits are the encoded bits
-    for (long unsigned int i = 0; i < encoded_bits.size(); i++)
-        bits.push_back(encoded_bits[i]);
+    };
 
+    add_bits(sfhIn.channels(), 16);
+    add_bits(padding, 16);
+    add_bits(q, 16);
+    add_bits(samples.size() / 2, 32);
+    add_bits(bs, 16);
+    add_bits(count_zeros, 16);
+    
+    if (!autoMode) {
+        m_vector.clear();
+        m_vector.push_back(m);
+    }
+    add_bits(m_vector.size(), 16);
+    
+    for (const auto &m_value : m_vector) {
+        add_bits(m_value, 16);
+    }
+
+    bits.insert(bits.end(), encoded_bits.begin(), encoded_bits.end());
     bitStream.writeBits(bits);
     bitStream.close();
 
-    //end the timer
+    // End the timer
     clock_t end = clock();
-    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
-    //convert the time to milliseconds
-    elapsed_secs = elapsed_secs * 1000;
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC * 1000;
     cout << "Execution time: " << elapsed_secs << " ms" << endl;
 
     return 0;
